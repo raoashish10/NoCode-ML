665d28a6194193bae34545f9b1cd0321acc6f303
Nishant213 <nishantnadkarni@gmail.com>
2020-09-13 06:03:36 +0530

    Added Naive Bayes to Classification.py

--- ML_Codes/Classification.py
+++ ML_Codes/Classification.py
@@ -138,3 +138,48 @@ def SVM(X_train,X_test,y_train,y_test):
     # plt.legend()
     # plt.show()
     return cm, accuracy, y_pred
+
+
+from sklearn.naive_bayes import GaussianNB
+def naive_bayes_algo(X_train,X_test,y_train,y_test):
+    classifier = GaussianNB()
+    classifier.fit(X_train, y_train)
+    
+    y_pred = classifier.predict(X_test)
+    
+    
+    cm = confusion_matrix(y_test, y_pred)
+    accuracy = accuracy_score(y_test, y_pred)
+    
+    # from matplotlib.colors import ListedColormap
+    # X_set, y_set = sc.inverse_transform(X_train), y_train
+    # X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 0.25),
+    #                      np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 0.25))
+    # plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+    #              alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+    # plt.xlim(X1.min(), X1.max())
+    # plt.ylim(X2.min(), X2.max())
+    # for i, j in enumerate(np.unique(y_set)):
+    #     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+    # plt.title('Naive Bayes (Training set)')
+    # plt.xlabel('Age')
+    # plt.ylabel('Estimated Salary')
+    # plt.legend()
+    # plt.show()
+    
+    # from matplotlib.colors import ListedColormap
+    # X_set, y_set = sc.inverse_transform(X_test), y_test
+    # X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 0.25),
+    #                      np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 0.25))
+    # plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+    #              alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+    # plt.xlim(X1.min(), X1.max())
+    # plt.ylim(X2.min(), X2.max())
+    # for i, j in enumerate(np.unique(y_set)):
+    #     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+    # plt.title('Naive Bayes (Test set)')
+    # plt.xlabel('Age')
+    # plt.ylabel('Estimated Salary')
+    # plt.legend()
+    # plt.show()
+    return cm, accuracy, y_pred
