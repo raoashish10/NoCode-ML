4f7980ae0bb67ac344b8bf5dc85426d3f9c6e36c
Nishant213 <nishantnadkarni@gmail.com>
2020-09-13 05:36:26 +0530

    Added Regression.py, Classification.py & Clustering.py

---
+++ ML_Codes/Classification.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 13 00:07:41 2020
+
+@author: nishantn
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, accuracy_score
+def logistic_regression(X_train,X_test,y_train,y_test):
+    classifier = LogisticRegression()
+    classifier.fit(X_train, y_train)
+    
+    y_pred = classifier.predict(X_test)
+    
+    
+    cm = confusion_matrix(y_test, y_pred)   # Print this in a fancy way plss
+    accuracy = accuracy_score(y_test, y_pred)
+    # from matplotlib.colors import ListedColormap
+    # X_set, y_set = sc.inverse_transform(X_train), y_train
+    # X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 0.25),
+    #                      np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 0.25))
+    # plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+    #              alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+    # plt.xlim(X1.min(), X1.max())
+    # plt.ylim(X2.min(), X2.max())
+    # for i, j in enumerate(np.unique(y_set)):
+    #     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+    # plt.title('Logistic Regression (Training set)')
+    # plt.xlabel('Age')
+    # plt.ylabel('Estimated Salary')
+    # plt.legend()
+    # plt.show()
+    
+    
+    # from matplotlib.colors import ListedColormap
+    # X_set, y_set = sc.inverse_transform(X_test), y_test
+    # X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 0.25),
+    #                      np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 0.25))
+    # plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+    #              alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+    # plt.xlim(X1.min(), X1.max())
+    # plt.ylim(X2.min(), X2.max())
+    # for i, j in enumerate(np.unique(y_set)):
+    #     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+    # plt.title('Logistic Regression (Test set)')
+    # plt.xlabel('Age')
+    # plt.ylabel('Estimated Salary')
+    # plt.legend()
+    # plt.show()
+    return cm, accuracy, y_pred
+
+from sklearn.neighbors import KNeighborsClassifier
+def KNN(X_train,X_test,y_train,y_test):
+    classifier = KNeighborsClassifier(n_neighbors = 5)
+    classifier.fit(X_train, y_train)
+    
+    y_pred = classifier.predict(X_test)
+    cm = confusion_matrix(y_test, y_pred)
+    accuracy = accuracy_score(y_test, y_pred)
+    # from matplotlib.colors import ListedColormap
+    # X_set, y_set = sc.inverse_transform(X_train), y_train
+    # X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 1),
+    #                      np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 1))
+    # plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+    #              alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+    # plt.xlim(X1.min(), X1.max())
+    # plt.ylim(X2.min(), X2.max())
+    # for i, j in enumerate(np.unique(y_set)):
+    #     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+    # plt.title('K-NN (Training set)')
+    # plt.xlabel('Age')
+    # plt.ylabel('Estimated Salary')
+    # plt.legend()
+    # plt.show()
+    
+    # X_set, y_set = sc.inverse_transform(X_test), y_test
+    # X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 1),
+    #                      np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 1))
+    # plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+    #              alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+    # plt.xlim(X1.min(), X1.max())
+    # plt.ylim(X2.min(), X2.max())
+    # for i, j in enumerate(np.unique(y_set)):
+    #     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+    # plt.title('K-NN (Test set)')
+    # plt.xlabel('Age')
+    # plt.ylabel('Estimated Salary')
+    # plt.legend()
+    # plt.show()
+    return cm, accuracy, y_pred
+
+
+from sklearn.svm import SVC
+def SVM(X_train,X_test,y_train,y_test):
+    classifier = SVC(kernel = 'linear', random_state = 0)
+    classifier.fit(X_train, y_train)
+    
+    y_pred = classifier.predict(X_test)
+    
+    cm = confusion_matrix(y_test, y_pred)
+    accuracy = accuracy_score(y_test, y_pred)
+    
+    # from matplotlib.colors import ListedColormap
+    # X_set, y_set = sc.inverse_transform(X_train), y_train
+    # X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 0.25),
+    #                      np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 0.25))
+    # plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+    #              alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+    # plt.xlim(X1.min(), X1.max())
+    # plt.ylim(X2.min(), X2.max())
+    # for i, j in enumerate(np.unique(y_set)):
+    #     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+    # plt.title('SVM (Training set)')
+    # plt.xlabel('Age')
+    # plt.ylabel('Estimated Salary')
+    # plt.legend()
+    # plt.show()
+    
+    # X_set, y_set = sc.inverse_transform(X_test), y_test
+    # X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 0.25),
+    #                      np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 0.25))
+    # plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+    #              alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+    # plt.xlim(X1.min(), X1.max())
+    # plt.ylim(X2.min(), X2.max())
+    # for i, j in enumerate(np.unique(y_set)):
+    #     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+    # plt.title('SVM (Test set)')
+    # plt.xlabel('Age')
+    # plt.ylabel('Estimated Salary')
+    # plt.legend()
+    # plt.show()
+    return cm, accuracy, y_pred
